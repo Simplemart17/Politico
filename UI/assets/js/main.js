e9f5a4984ecfed962a6867c77d8c12fa7800fc37
Martins Aloba <aloba.martins@gmail.com>
2019-01-22 19:17:02 +0100

    feat(dashboard): add expression of interest form
    add expression of interest form for politician
    
    Closes #163329251

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,4 +1,19 @@
-//Reset form function
 const myFunction = () => {
     document.getElementById("myForm").reset();
+}
+
+const modal = document.getElementById('modal');
+
+const openModal = () => {
+    document.getElementById('modal').style.display = 'block';
+}
+
+const closeModal = () => {
+    document.getElementById('modal').style.display='none'
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
