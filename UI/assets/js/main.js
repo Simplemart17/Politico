5c42b335a988c8bc4d6c5d55f63efc0dcf0828a0
Martins Aloba <aloba.martins@gmail.com>
2019-01-23 12:40:34 +0100

    feat(dashboard): add admin panel
    add page for admin to delete a political party
    add page for admin to create a political party
    add page for admin to edit name of a party
    add page for admin to create political office
    
    Closes #163328899, #163328615, #163328257, #163328080

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,4 +1,53 @@
-//Reset form function
 const myFunction = () => {
     document.getElementById("myForm").reset();
+}
+
+const modal = document.getElementById('modal');
+
+const openModal = () => {
+    document.getElementById('modal').style.display = 'block';
+}
+
+const closeModal = () => {
+    document.getElementById('modal').style.display='none'
+}
+
+const openPartyModal = () => {
+    document.getElementById('partyModal').style.display = 'block';
+}
+
+const closePartyModal = () => {
+    document.getElementById('partyModal').style.display='none'
+}
+
+const openOfficeModal = () => {
+    document.getElementById('officeModal').style.display = 'block';
+}
+
+const closeOfficeModal = () => {
+    document.getElementById('officeModal').style.display='none'
+}
+
+const openEditModal = () => {
+    document.getElementById('editModal').style.display = 'block';
+}
+
+const closeEditModal = () => {
+    document.getElementById('editModal').style.display='none'
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// let test = document.getElementsByClassName('box');
+
+// console.log(test);
+
+const removeParty = () => {
+    let elem = document.getElementById('remove');
+    elem.parentNode.removeChild(elem);
 }
