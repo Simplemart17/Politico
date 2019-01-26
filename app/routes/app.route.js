38032fd12753fc36a76da69e1c01db6ed1368c71
Martins Aloba <aloba.martins@gmail.com>
2019-01-26 15:06:35 +0100

    feat(api): add create office route
    add api endpoint route to create a political office
    modify the pull_request_template.md
    modify app.js file to include the new route
    modify officedb.js with more dummy
    
    Closes #163446297

--- app/routes/app.route.js
+++ app/routes/app.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import partyController from '../controllers/partyController';
+import officeController from '../controllers/officeController';
 import validation from '../middleware/validation';
 
 const router = express.Router();
@@ -9,5 +10,7 @@ router.get('/party', partyController.getAllParty);
 router.get('/party/:id', partyController.getParty);
 
 router.post('/party', validation.input, partyController.createParty);
+
+router.post('/office', officeController.createOffice);
 
 export default router;
