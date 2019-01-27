1f6ae230c6b2a445dc625e99e34ee419dc0bc3db
Martins Aloba <aloba.martins@gmail.com>
2019-01-27 16:22:15 +0100

    fix(dashboard): modify javascript to redirect pages
    refactor javascript code to redirect signin pages
    
    Closes #163500146

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,86 +1,65 @@
-const myFunction = () => {
-    document.getElementById("myForm").reset();
-}
+const reset = () => {
+  document.getElementById('form').reset();
+};
 
 const modal = document.getElementById('modal');
 
 const openModal = () => {
-    document.getElementById('modal').style.display = 'block';
-}
+  document.getElementById('modal').style.display = 'block';
+};
 
 const closeModal = () => {
-    document.getElementById('modal').style.display='none'
-}
+  document.getElementById('modal').style.display = 'none';
+};
 
 const openPetitionModal = () => {
-    document.getElementById('petitionModal').style.display = 'block';
-}
+  document.getElementById('petitionModal').style.display = 'block';
+};
 
 const closePetitionModal = () => {
-    document.getElementById('petitionModal').style.display='none'
-}
+  document.getElementById('petitionModal').style.display = 'none';
+};
 
 const openPartyModal = () => {
-    document.getElementById('partyModal').style.display = 'block';
-}
+  document.getElementById('partyModal').style.display = 'block';
+};
 
 const closePartyModal = () => {
-    document.getElementById('partyModal').style.display='none'
-}
+  document.getElementById('partyModal').style.display = 'none';
+};
 
 const openOfficeModal = () => {
-    document.getElementById('officeModal').style.display = 'block';
-}
+  document.getElementById('officeModal').style.display = 'block';
+};
 
 const closeOfficeModal = () => {
-    document.getElementById('officeModal').style.display='none'
-}
+  document.getElementById('officeModal').style.display = 'none';
+};
 
 const openEditModal = () => {
-    document.getElementById('editModal').style.display = 'block';
-}
+  document.getElementById('editModal').style.display = 'block';
+};
 
 const closeEditModal = () => {
-    document.getElementById('editModal').style.display='none'
-}
+  document.getElementById('editModal').style.display = 'none';
+};
 
 const openResultModal = () => {
-    document.getElementById('resultModal').style.display = 'block';
-}
+  document.getElementById('resultModal').style.display = 'block';
+};
 
 const closeResultModal = () => {
-    document.getElementById('resultModal').style.display='none'
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-// let test = document.getElementsByClassName('box');
-
-// console.log(test);
+  document.getElementById('resultModal').style.display = 'none';
+};
 
 const removeParty = () => {
-    let elem = document.getElementById('remove');
-    elem.parentNode.removeChild(elem);
-}
-
+  const elem = document.getElementById('remove');
+  elem.parentNode.removeChild(elem);
+};
 
 const adminLogin = () => {
-    if((document.getElementById('password').value == 'admin') && (document.getElementById('username').value == 'admin')) {
-       location.href = "admin.html";
-    } else {
-        location.href='politician-profile.html';
-    }
-}
-
-const adminPanel = () => {
-    if ((document.getElementById('password').value == 'admin') && (document.getElementById('username').value == 'admin')) {
-        location.href = "admin.html";
-    } else {
-        location.href='citizen-profile.html';
-    }
-}
+  if ((document.getElementById('password').value === 'admin') && (document.getElementById('username').value === 'admin')) {
+    location.href = 'admin.html';
+  }
+  return null;
+};
