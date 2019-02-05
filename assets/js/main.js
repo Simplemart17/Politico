d8ca98de1e0e8e3fc8da81c8a453e33a11af8b26
Martins Aloba <aloba.martins@gmail.com>
2019-02-05 21:55:05 +0100

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,14 +64,25 @@ const adminLogin = () => {
   return null;
 };
 
-const login = () => {
+
+const signUp = () => {
   event.preventDefault();
-  const url = 'https://mart-politico-app.herokuapp.com/api/v1/auth/login';
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const url = 'http://localhost:8000/api/v1/auth/signup';
+  const firstname = document.getElementById('user_firstname').value;
+  const lastname = document.getElementById('user_lastname').value;
+  const othername = document.getElementById('user_othername').value;
+  const email = document.getElementById('user_email').value;
+  const username = document.getElementById('user_username').value;
+  const phonenumber = document.getElementById('user_phonenumber').value;
+  const password = document.getElementById('user_password').value;
 
   const data = {
+    firstname,
+    lastname,
+    othername,
     email,
+    username,
+    phoneNumber: phonenumber,
     password,
   };
   const request = new Request(url, {
@@ -82,16 +93,15 @@ const login = () => {
       'Content-Type': 'application/json',
     },
   });
-  async function postLogin(payLoad) {
+  async function postSignup(payLoad) {
     try {
       const resp = await fetch(payLoad);
       const data = await resp.json();
-      console.log(data);
-      //   window.location.href = '/index.hmtl';
-      return resp.status;
+      window.location.href = 'index.html';
     } catch (err) {
       throw err;
     }
   }
-  postLogin(request);
+
+  postSignup(request);
 };
