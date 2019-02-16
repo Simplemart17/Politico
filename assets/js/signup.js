2f42c28979c740cac307cdf48c7aae6423cf8a0c
Martins Aloba <aloba.martins@gmail.com>
2019-02-16 21:43:32 +0100

    Updates

---
+++ assets/js/signup.js
@@ -0,0 +1,50 @@
+const baseUrl = 'https://mart-politico-app.herokuapp.com';
+const url = 'http://localhost:8000/api/v1/auth/signup';
+const signUpForm = document.getElementById('form_register');
+
+// Function to register an account
+signUpForm.onsubmit = () => {
+  event.preventDefault();
+
+  const firstname = document.getElementById('user_firstname').value;
+  const lastname = document.getElementById('user_lastname').value;
+  const othername = document.getElementById('user_othername').value;
+  const email = document.getElementById('user_email').value;
+  const phoneNumber = document.getElementById('user_phonenumber').value;
+  const password = document.getElementById('user_password').value;
+
+  const regForm = {
+    firstname,
+    lastname,
+    othername,
+    email,
+    phoneNumber,
+    password,
+  };
+  fetch(`${baseUrl}/api/v1/auth/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(regForm),
+  })
+    .then(response => response.json())
+    .then((resp) => {
+      if (resp.status === 201) {
+        const token = resp.data[0].token;
+        localStorage.setItem('token', token);
+        setTimeout(() => {
+          window.location.href = 'citizen-profile.html';
+        }, 3000);
+      }
+      if (resp.error === 'Email already exist!') {
+        console.log(resp.error);
+      }
+      if (resp.status === 400) {
+        console.log(resp.status);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
