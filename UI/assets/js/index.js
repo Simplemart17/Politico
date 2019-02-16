8a5a3ccb7b6014ec55533efa6d8aebc3dcb3703b
Martins Aloba <aloba.martins@gmail.com>
2019-02-16 22:43:49 +0100

    #163803858 Consume parties endpoint  (#33)
    * feat(api): consume parties endpoint
    
    add function to get all parties using fetch api
    add function to delete party by id using fetch api
    add function to modify party name using fetch api
    
    [Finishes #163803858]

--- UI/assets/js/index.js
+++ UI/assets/js/index.js
@@ -1,104 +1,85 @@
-const signUp = () => {
-  event.preventDefault();
-  const url = 'https://mart-politico-app.herokuapp.com/api/v1/auth/signup';
-  const firstname = document.getElementById('user_firstname').value;
-  const lastname = document.getElementById('user_lastname').value;
-  const othername = document.getElementById('user_othername').value;
-  const email = document.getElementById('user_email').value;
-  const phonenumber = document.getElementById('user_phonenumber').value;
-  const password = document.getElementById('user_password').value;
-  const errMsg = document.getElementById('err_msg');
-  const errHeader = document.getElementById('error_header');
+let id;
 
-  const data = {
-    firstname,
-    lastname,
-    othername,
-    email,
-    phoneNumber: phonenumber,
-    password,
-  };
-  const request = new Request(url, {
-    method: 'post',
-    body: JSON.stringify(data),
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-  });
-  async function postSignup(payLoad) {
-    try {
-      const response = await fetch(payLoad);
-      const data = await response.json();
-      if (data.error === 'Email already exist!') {
-        errMsg.innerHTML = data.error;
-      }
-      if (response.status === 400) {
-        errHeader.innerHTML = 'The request cannot be completed!';
-        return response.status;
-      }
-      if (response.status === 201) {
-        document.getElementById('success_header').innerHTML = 'You have successfully signup!';
-        setTimeout(() => {
-          window.location.href = 'citizen-profile.html';
-        }, 2000);
-      }
-    } catch (err) {
-      throw err;
-    }
-  }
-  postSignup(request);
+const reset = () => {
+  document.getElementById('form').reset();
 };
 
-const url = 'https://mart-politico-app.herokuapp.com/api/v1/auth/login';
-const signinForm = document.getElementById('signin-form');
+const modal = document.getElementById('modal');
 
-signinForm.onsubmit = () => {
-  event.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const emailError = document.getElementById('email_error');
-  const passwordError = document.getElementById('password_error');
-  const errHeader = document.getElementById('error_header');
-  const login = {
-    email,
-    password,
-  };
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(login),
-  })
-    .then(response => response.json())
-    .then((resp) => {
-      const isadmin = resp.data[0].user.isadmin;
-      if (resp.error === 'Incorrect email address') {
-        emailError.innerHTML = 'Incorrect email address';
-      }
-      if (resp.error === 'Incorrect password!') {
-        passwordError.innerHTML = 'Incorrect password';
-      }
-      if (resp.message === 'You have successfully signed in!') {
-        const token = resp.data[0].token;
-        localStorage.setItem('token', token);
-        if (isadmin === true) {
-          document.getElementById('success').style.display = 'block';
-          setTimeout(() => {
-            window.location.href = 'admin.html';
-          }, 3000);
-        } else {
-          document.getElementById('success').style.display = 'block';
-          setTimeout(() => {
-            window.location.href = 'citizen-profile.html';
-          }, 3000);
-        }
-      } else {
-        errHeader.innerHTML = 'You cannot be logged in, try again!';
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const openModal = () => {
+  document.getElementById('modal').style.display = 'block';
+};
+
+const closeModal = () => {
+  document.getElementById('modal').style.display = 'none';
+};
+
+const openPetitionModal = () => {
+  document.getElementById('petitionModal').style.display = 'block';
+};
+
+const closePetitionModal = () => {
+  document.getElementById('petitionModal').style.display = 'none';
+};
+
+const openPartyModal = () => {
+  document.getElementById('partyModal').style.display = 'block';
+};
+
+const closePartyModal = () => {
+  document.getElementById('partyModal').style.display = 'none';
+};
+
+const openOfficeModal = () => {
+  document.getElementById('officeModal').style.display = 'block';
+};
+
+const closeOfficeModal = () => {
+  document.getElementById('officeModal').style.display = 'none';
+};
+
+const openEditModal = (partyId) => {
+  id = partyId;
+  document.getElementById('editModal').style.display = 'block';
+};
+
+const closeEditModal = () => {
+  document.getElementById('editModal').style.display = 'none';
+};
+
+const openDeleteModal = (partyId) => {
+  id = partyId;
+  document.getElementById('deleteModal').style.display = 'block';
+};
+
+const closeDeleteModal = () => {
+  document.getElementById('deleteModal').style.display = 'none';
+};
+
+const openResultModal = () => {
+  document.getElementById('resultModal').style.display = 'block';
+};
+
+const closeResultModal = () => {
+  document.getElementById('resultModal').style.display = 'none';
+};
+
+// const removeParty = () => {
+//   const elem = document.getElementById('remove');
+//   elem.parentNode.removeChild(elem);
+//   closeDeleteModal();
+// };
+
+const passwordCheck = () => {
+  const password1 = document.getElementById('user_password').value;
+  const password2 = document.getElementById('user_confirm_password').value;
+  if (password1 !== password2) {
+    document.getElementById('user_confirm_password').style.borderColor = 'red';
+    document.getElementById('password_error_msg').innerHTML = 'Password do not match!';
+    document.getElementById('register_btn').disabled = true;
+  } else {
+    document.getElementById('user_confirm_password').style.borderColor = 'green';
+    document.getElementById('password_error_msg').innerHTML = '';
+    document.getElementById('register_btn').disabled = false;
+  }
 };
---
+++ UI/assets/js/signin.js
@@ -0,0 +1,49 @@
+const baseUrl = 'https://mart-politico-app.herokuapp.com';
+const url = 'http://localhost:8000';
+const signinForm = document.getElementById('signin-form');
+
+signinForm.onsubmit = () => {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const emailError = document.getElementById('email_error');
+  const passwordError = document.getElementById('password_error');
+  const errHeader = document.getElementById('error_header');
+  const login = {
+    email,
+    password,
+  };
+  fetch(`${baseUrl}/api/v1/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(login),
+  })
+    .then(response => response.json())
+    .then((resp) => {
+      console.log(resp);
+      const isadmin = resp.data[0].user.isadmin;
+      if (resp.error === 'Incorrect email address') {
+        emailError.innerHTML = 'Incorrect email address';
+      }
+      if (resp.error === 'Incorrect password!') {
+        passwordError.innerHTML = 'Incorrect password';
+      }
+      if (resp.message === 'You have successfully signed in!') {
+        const token = resp.data[0].token;
+        localStorage.setItem('token', token);
+        localStorage.setItem('isadmin', isadmin);
+        document.getElementById('success').style.display = 'block';
+        console.log(resp);
+        setTimeout(() => {
+          (isadmin === true) ? window.location.href = 'admin.html' : window.location.href = 'citizen-profile.html';
+        }, 3000);
+      } else {
+        errHeader.innerHTML = 'You cannot be logged in, try again!';
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
