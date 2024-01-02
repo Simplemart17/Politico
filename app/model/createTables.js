dcec81a268320e01d6bc5edc799f999b9019f128
Martins Aloba <aloba.martins@gmail.com>
2024-01-02 00:30:34 -0500

    add more test data

--- app/model/createTables.js
+++ app/model/createTables.js
@@ -9,9 +9,14 @@ import {
 import { generateHashPassword } from '../middleware/Helper';
 import db from './db';
 
+const date = new Date();
+// Add a day
+date.setDate(date.getDate() + 1);
+
 const adminPassword = generateHashPassword('admin');
 const testPassword = generateHashPassword('test');
-const resetToken = '687810e6-0b33-4ae1-841d-fb23983a346f';
+const resetToken1 = 'edad3fcc-aec0-4ece-af7e-f5867cef14a3';
+const resetToken2 = '687810e6-0b33-4ae1-841d-fb23983a346f';
 
 const addAdmin = () => ({
   text: 'INSERT INTO users(firstname, lastname, othername, phoneNumber, email, isAdmin, password, passportUrl) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
@@ -25,7 +30,12 @@ const addTestAdmin = () => ({
 
 const addTestDetails = () => ({
   text: 'INSERT INTO users(firstname, lastname, othername, phoneNumber, email, isAdmin, password, passportUrl, passwordResetToken, passwordResetExpires) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
-  values: ['Test', 'Database', 'Tester', '08012345678', 'reset@politico.com', 'false', testPassword, 'wwww.image.com', resetToken, new Date('2023-12-12')],
+  values: ['Test', 'Database', 'Tester', '08012345678', 'reset@politico.com', 'false', testPassword, 'wwww.image.com', resetToken2, new Date('2023-12-12')],
+});
+
+const passwordResetTestDetails = () => ({
+  text: 'INSERT INTO users(firstname, lastname, othername, phoneNumber, email, isAdmin, password, passportUrl, passwordResetToken, passwordResetExpires) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+  values: ['Test2', 'Testing', 'Tester', '08012345679', 'reset1@politico.com', 'false', testPassword, 'wwww.image.com', resetToken1, date],
 });
 
 async function createAllTables() {
@@ -38,6 +48,7 @@ async function createAllTables() {
   await db.query(addAdmin());
   await db.query(addTestAdmin());
   await db.query(addTestDetails());
+  await db.query(passwordResetTestDetails());
 }
 
 createAllTables()
