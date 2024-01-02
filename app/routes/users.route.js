8fbef9bc17b940d3da0cb8f81760c0b4ac7c3af3
Martins Aloba <aloba.martins@gmail.com>
2024-01-01 23:50:13 -0500

    modify user route file

--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -6,13 +6,9 @@ import validation from '../middleware/validation';
 const userRouter = express.Router();
 
 userRouter.post('/auth/signup', validation.signUp, users.signUpUsers);
-
 userRouter.post('/auth/login', validation.signIn, users.userSignIn);
-
 userRouter.get('/auth/profile', Auth.verifyToken, users.getUser);
-
 userRouter.post('/auth/forgot-password', users.forgotPassword);
-
 userRouter.get('/auth/reset-password/:resetToken', users.resetPassword);
 
 export default userRouter;
